70dc37bad1d7e0381741e1187afe314d3b3c7d3b
Blake-McCullough <blake.mccullough@iinet.net.au>
2022-09-07 12:04:43 +0800

    Removed unneccessary discord bot.

--- main.py
+++ main.py
@@ -1,18 +1,10 @@
 from dotenv import load_dotenv
 from discord_server_link import  run_edits
 from teams_discord import start_up
-import web_server,discord_bot
-from multiprocessing import Process
-def WebServer():
-    web_server.start()
-    
-def DiscordBot():
-    discord_bot.start()
 
 
-#For event that occurs every x minutes.
-import time
-import atexit
+
+
 
 from apscheduler.schedulers.background import BackgroundScheduler
 
@@ -29,11 +21,7 @@ if __name__ == "__main__":
     
     load_dotenv()
     start_up()
-    #For running the bot and server at same time.
-    #p1 = Process(target =  WebServer)
     WebServer()
-    #p1.start()
-    #p2 = Process(target = DiscordBot)
-    #p2.start()
+
     
     
